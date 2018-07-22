3abc57b86a6c172ff8af56d7343d6c622d74a0a6
luzhongkuan <zhongkuan.lu@vhuomai.com>
2018-07-22 20:58:52 +0800

    fix

--- src/lib/wx/wxPay.js
+++ src/lib/wx/wxPay.js
@@ -14,31 +14,6 @@ const onBridgeReady = new Promise(resolve => {
   }
 });
 
-export function wechatPay(config) {
-  console.info('wechatPay', config);
-  return new Promise(async (resolve, reject) => {
-    await onBridgeReady;
-    // eslint-disable-next-line
-    WeixinJSBridge.invoke('getBrandWCPayRequest', config, res => {
-      const { err_msg = '' } = res;
-      const isSuccess = err_msg === 'get_brand_wcpay_request:ok';
-      const isCancel = err_msg === 'get_brand_wcpay_request:cancel';
-      const isFail = err_msg === 'get_brand_wcpay_request:fail';
-      if (isSuccess) {
-        resolve('success');
-        return;
-      } else if (isCancel) {
-        resolve('cancel');
-        return;
-      } else if (isFail) {
-        resolve('fail');
-        return;
-      }
-      reject();
-    });
-  });
-}
-
 export function chooseWXPay(config) {
   return new Promise(async resolve => {
     await loadWechatJSSDK();
@@ -66,5 +41,30 @@ export function chooseWXPay(config) {
     );
     console.info('chooseWXPay', option);
     wx.chooseWXPay(option);
+  });
+}
+
+export default function wechatPay(config) {
+  console.info('wechatPay', config);
+  return new Promise(async (resolve, reject) => {
+    await onBridgeReady;
+    // eslint-disable-next-line
+    WeixinJSBridge.invoke('getBrandWCPayRequest', config, res => {
+      const { err_msg = '' } = res;
+      const isSuccess = err_msg === 'get_brand_wcpay_request:ok';
+      const isCancel = err_msg === 'get_brand_wcpay_request:cancel';
+      const isFail = err_msg === 'get_brand_wcpay_request:fail';
+      if (isSuccess) {
+        resolve('success');
+        return;
+      } else if (isCancel) {
+        resolve('cancel');
+        return;
+      } else if (isFail) {
+        resolve('fail');
+        return;
+      }
+      reject();
+    });
   });
 }
