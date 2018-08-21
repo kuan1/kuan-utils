1c11090eec3730437b105fc123bc863cb8cd9491
luzhongkuan <luzhongk@126.com>
2018-08-21 16:19:55 +0800

    isImage

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -2,25 +2,25 @@
  * @desc   判断浏览器类型
  * @return {Boolean}
  */
-let userAgent = '';
+let userAgent = ''
 if (typeof navigator !== 'undefined') {
-  userAgent = navigator.userAgent.toLowerCase();
+  userAgent = navigator.userAgent.toLowerCase()
 }
 
 // ios浏览器
-export const isiOS = userAgent.indexOf('applewebkit') >= 0;
+export const isiOS = userAgent.indexOf('applewebkit') >= 0
 
 // 微信浏览器
-export const isWx = userAgent.indexOf('micromessenger') > -1;
+export const isWx = userAgent.indexOf('micromessenger') > -1
 
 // 支付宝
-export const isAliPay = userAgent.indexOf('alipayclient') >= 0;
+export const isAliPay = userAgent.indexOf('alipayclient') >= 0
 
 /**
  * @desc   判断是否pc页面
  * @return {Boolean}
  */
-export const isPc = !(/Android|webOS|iPhone|iPod|BlackBerry/i.test(userAgent));
+export const isPc = !(/Android|webOS|iPhone|iPod|BlackBerry/i.test(userAgent))
 
 /**
  * @desc   判断是否为手机号
@@ -28,7 +28,7 @@ export const isPc = !(/Android|webOS|iPhone|iPod|BlackBerry/i.test(userAgent));
  * @return {Boolean}
  */
 function isPhone(str) {
-  return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str);
+  return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str)
 }
 
 /**
@@ -37,7 +37,7 @@ function isPhone(str) {
  * @return {Boolean}
  */
 export function isNum(str) {
-  return /^[0-9]+([.]{1}[0-9]+){0,1}$/.test(str);
+  return /^[0-9]+([.]{1}[0-9]+){0,1}$/.test(str)
 }
 
 /**
@@ -46,7 +46,7 @@ export function isNum(str) {
  * @return {Boolean}
  */
 export function isEmail(str) {
-  return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str);
+  return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str)
 }
 
 /**
@@ -56,7 +56,7 @@ export function isEmail(str) {
  * @return {Boolean}
  */
 export function isIdCard(str) {
-  return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str);
+  return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str)
 }
 
 /**
@@ -64,6 +64,16 @@ export function isIdCard(str) {
  * @param  {String} str
  * @return {Boolean}
  */
-function isUrl(str) {
-  return /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i.test(str);
+export function isUrl(str) {
+  return /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i.test(str)
+}
+
+/**
+ * @param url 图片地址
+ * @returns {boolean}
+ */
+export function isImage(url) {
+  if (!url) return false
+  const suffix = url.substr(url.lastIndexOf('.') + 1)
+  return ['png', 'gif', 'svg', 'jpg', 'icon', 'jpeg'].indexOf(suffix.substr(0, suffix.indexOf('?'))) > -1
 }
