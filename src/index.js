c9e38dfc5e96e9048dd00c1d64e29b9739e8f19e
luzhongkuan <luzhongk@126.com>
2018-08-23 10:56:15 +0800

    顺序

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // 获取全部utils
+// import jsonToExcel from './lib/jsonToExcel' // 导出excel
 import {throttle, debounce, deepClone, randomColor} from './lib/tools' // 工具类
 import * as qs from './lib/qs' // url参数
 import * as timeUtils from './lib/timeUtils' // 时间工具类
@@ -6,11 +7,11 @@ import * as cookie from './lib/cookie' // cookie工具类
 import * as math from './lib/math' // js精度计算
 import localData from './lib/localData' // localStorage工具类
 import copy from './lib/copy' // 复制
-// import jsonToExcel from './lib/jsonToExcel' // 导出excel
 import download from './lib/download' // 下载
 import media from './lib/media'
 
 export {
+  // jsonToExcel,
   throttle, // 函数节流
   debounce, // 函数防抖
   deepClone, // 深拷贝
@@ -21,7 +22,6 @@ export {
   math,
   localData,
   copy,
-  // jsonToExcel,
   download,
   media
 }
