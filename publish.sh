f85545fb01c090d8ac56986e41f674bb9cfc62ad
luzhongkuan <luzhongk@126.com>
2018-10-21 14:08:10 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 echo starting npm version $1 ...; # major、minor、patch
 
-order_list=(major、minor、patch)
+order_list=(major、minor、patch);
 
 echo "${order_list[@]}" | grep -wq $1 &&  npm version $1 || echo "No";
+
+npm run build;
