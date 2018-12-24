e6198f64034184e4e2d117c2718346f0966ad516
luzhongkuan <luzhongk@126.com>
2018-12-24 16:20:16 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -1,7 +1,16 @@
-echo starting npm version $1 ...; # major、minor、patch
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+echo "start build ...";
+
+npm run build ;
+git add . ;
+git commit -a - m 'pre publish';
+
+echo "start npm version $1 ..."; # major、minor、patch
 
 order_list=(major、minor、patch);
 
 echo "${order_list[@]}" | grep -wq $1 &&  npm version $1 || echo "No";
-
-npm run build;
