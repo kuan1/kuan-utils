8e0fd978c26cb8803d48af64bdf479ce1c2d66db
luzhongkuan <luzhongk@126.com>
2019-06-03 00:29:09 +0800

    fix

--- build/build.js
+++ build/build.js
@@ -1,37 +1,22 @@
 const { buildLib } = require('kuan-pack')
-const nodeExternals = require('webpack-node-externals')
-
-
+const config = require('kuan-pack/build/webpack.lib.min.conf')
 const { fileDisplay, resolve } = require('./utils')
+
+const targets = fileDisplay(resolve('src'))
+
+const entry = {}
+targets.forEach(item => {
+  const fileName = item.replace('/', '').replace('.js', '')
+  const name = fileName === 'index' ? 'kuan-utils' : fileName
+  entry[name] = resolve(`src${item}`)
+})
 
 buildLib({
   extend(config) {
     config.output.path = resolve('lib')
-    config.externals = nodeExternals({ importType: 'umd' })
-  }
+    config.output.filename = '[name].js'
+    config.output.library = '[name]'
+    config.entry = entry
+  },
+  config
 })
-
-// const { buildLib } = require('kuan-pack')
-// const config = require('kuan-pack/build/webpack.lib.min.conf')
-// const { fileDisplay, resolve } = require('./utils')
-
-// const targets = fileDisplay(resolve('src'))
-
-// const entry = {}
-// targets.forEach(item => {
-//   const fileName = item.replace('/', '').replace('.js', '')
-//   const name = fileName === 'index' ? 'kuan-utils' : fileName
-//   entry[name] = resolve(`src${item}`)
-// })
-
-// buildLib({
-//   extend(config) {
-//     config.output.path = resolve('lib')
-//     config.output.filename = '[name].js'
-//     config.output.library = '[name]'
-//     config.output.globalObject = 'typeof self !== \'undefined\' ? self : this'
-//     delete config.output.umdNamedDefine
-//     config.entry = entry
-//   },
-//   config
-// })
