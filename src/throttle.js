20a58bc466fb71f91888020927c9e2226342e8c8
luzhongkuan <luzhongk@126.com>
2019-06-13 21:26:08 +0800

    fix throttle

--- src/throttle.js
+++ src/throttle.js
@@ -1,13 +1,19 @@
-// 节流函数
-let startTime = new Date()
+let timer
+
+/**
+ * @description: 函数节流
+ * @param {fn} Function
+ * @param {time} Number 截流时间
+ */
 export function throttle(fn, time = 300) {
   if (typeof fn !== 'function') {
     throw new Error('必须传入一个函数作为参数')
   }
-  const currentTime = new Date()
-  if (currentTime - startTime > time) {
-    startTime = currentTime
-    fn()
+  if (!timer) {
+    timer = setTimeout(() => {
+      fn()
+      timer = null
+    }, time)
   }
 }
 
