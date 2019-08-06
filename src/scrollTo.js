9ce153bfbb21545aa6d354f4a07b68e5393e5f99
luzhongkuan <luzhongk@126.com>
2019-08-06 20:46:32 +0800

    fix scroll

--- src/scrollTo.js
+++ src/scrollTo.js
@@ -8,6 +8,8 @@ export function scrollToTranstion(top = 0) {
   const c = document.documentElement.scrollTop || document.body.scrollTop
 
   const distance = c - top
+  // 到达目标值或者滚动达不到目标停止
+  if (Math.abs(distance) <= 1) return
 
   let y = c - distance / 10
   if (Math.abs(distance) / 10 < 1) {
@@ -15,6 +17,7 @@ export function scrollToTranstion(top = 0) {
   }
 
   window.scrollTo(0, y)
+  if (y <= 0 && top <= 0) return
   timer = requestAnimationFrame(() => {
     scrollToTranstion(top)
   })
