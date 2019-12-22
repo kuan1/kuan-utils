41de90d5e4b071816cee36966103d88bfbd16a8f
luzhongkuan <luzhongk@126.com>
2019-12-22 18:18:32 +0800

    feat: debounce

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,6 @@ import color from "./color";
 import cookie from "./cookie";
 import copy from "./copy";
 import dayjs from "./dayjs";
+import debounce from "./debounce";
 
-export default { color, cookie, copy, dayjs };
+export default { color, cookie, copy, dayjs, debounce };
